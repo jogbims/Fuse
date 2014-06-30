e4866f31175df437d54327a9d7dc0a3ec2683fe2
Jonathan Ogbimi <jonathan.o@vas2nets.com>
2014-06-30 15:01:02 +0100

    Updated the Constants

--- src/com/vas2nets/fuse/util/Constants.java
+++ src/com/vas2nets/fuse/util/Constants.java
@@ -7,6 +7,6 @@ public class Constants {
 	public static final String SENDING_SMS = "Sending SMS...";
 	public static final String NO_INTERNET_ACTIVITY = "No Internet Connectivity....try again!!!";
 	public static final String SMS_WILL_BE_SENT = "SMS will be sent to this number ";
-	public static final String PIN_VERIFICATION_FAILED = "Pin Verification Failed....try again!!!";
+	public static final String PIN_VERIFICATION_FAILED = "Pin Verification Failed..try again!!!";
 	public static int SPLASH_TIME_OUT = 3000;
 }
